14bcdc77c8d85a5b13fbf7b25150da8f75414ded
mohit <mohitmodh10@gmail.com>
2024-10-31 17:04:39 -0400

    feat: completed HomeScreen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "835464882"
+    serviceWorkerVersion: "1077377226"
   }
 });
